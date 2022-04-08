f87f299b50918880343c276eff5f4f7dab873ab6
Elijah Luckey <luckeyelijah112@gmail.com>
2022-04-08 12:10:00 -0400

    Updates hooks

--- console_program/hooks/post_gen.dart
+++ console_program/hooks/post_gen.dart
@@ -3,15 +3,15 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
-  final path = '${Directory.current.path}/${context.vars['name'] as String}';
+  final name = context.vars['name'] as String;
   final pubGet = context.logger.progress(
-    'Running `dart pub get --directory=$path`',
+    'Running `dart pub get --directory=$name`',
   );
 
-  await Process.run('dart', ['pub', 'get', '--directory=$path']);
-  pubGet('Finished `dart pub get --directory=$path`');
+  await Process.run('dart', ['pub', 'get', '--directory=$name']);
+  pubGet('Finished `dart pub get --directory=$name`');
 
-  final dartFmt = context.logger.progress('Running `dart format $path`');
-  await Process.run('dart', ['format', '$path']);
-  dartFmt('Finished `dart format $path`');
+  final dartFmt = context.logger.progress('Running `dart format $name`');
+  await Process.run('dart', ['format', name]);
+  dartFmt('Finished `dart format $name`');
 }
