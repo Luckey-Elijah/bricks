0dc4ceda3a971e15da1b6d2d555849472e68f523
Elijah Luckey <luckeyelijah112@gmail.com>
2022-07-08 09:56:06 -0400

    remove deprecated call

--- console_program/hooks/post_gen.dart
+++ console_program/hooks/post_gen.dart
@@ -4,14 +4,14 @@ import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
   final name = context.vars['name'] as String;
-  final pubGet = context.logger.progress(
+
+  final cleanUp = context.logger.progress(
     'Running `dart pub get --directory=$name`',
   );
 
   await Process.run('dart', ['pub', 'get', '--directory=$name']);
-  pubGet('Finished `dart pub get --directory=$name`');
+  cleanUp.update('Finished `dart pub get --directory=$name`');
 
-  final dartFmt = context.logger.progress('Running `dart format $name`');
   await Process.run('dart', ['format', name]);
-  dartFmt('Finished `dart format $name`');
+  cleanUp.complete('Finished `dart format $name`');
 }
